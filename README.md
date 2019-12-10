77dd74eb920687ffe28b6261c947c32b8e0ba818
chrunlee <chrunlee@foxmail.com>
2019-12-10 15:40:31 +0800

    Less middleware of thinkjs 3.0

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # think-less
-A less middleware for thinkjs 3.0
+A less middleware for thinkjs 3.0 on development enviroment.
+Compiler less file to css when request `xxx.css` file in pages.
+
+# Installation
+```
+npm install think-less --save-dev
+```
+
+# API
+
+Modify `/src/config/middleware.js` 
+
+```
+const less = require('think-less');
+const path = require('path');
+const isDev = think.env === 'development';
+
+module.exports = [
+    {
+        handle: less,
+        enable: isDev ? true : false,
+        options: {
+            root: path.join(think.ROOT_PATH, 'res'),
+            target: path.join(think.ROOT_PATH, 'www'),
+            lessOptions: {
+                paths: [path.join(think.ROOT_PATH, 'res')],
+                compress: true
+            }
+        }
+    }
+]
+```
+
+### Options 
+
+- root : Root directory to find less file .
+- target : Root directory to place css file .
+- lessOptions : Options of [https://github.com/less/less.js](Less.js)
---
+++ index.js
@@ -0,0 +1,28 @@
+//thinkjs middleware of less compiler
+const less = require('less');
+const path = require('path');
+const fs = require('fs');
+const mkdirsp = require('mkdirsp');
+
+module.exports = (options,app)=>{
+    return async (ctx,next)=>{
+        let extname = path.extname(ctx.path);
+        if(extname.toLowerCase() != '.css'){
+            return next();
+        }
+        let filePath = path.join(options.rootpath,ctx.path.substr(0,ctx.path.indexOf('.css'))+'.less');
+        let targetPath = path.join(options.target,ctx.path);
+        if(!fs.existsSync(filePath)){
+            return next();
+        }
+        let content = Buffer.from(fs.readFileSync(filePath)).toString('utf8');
+        try{
+            await mkdirsp(path.dirname(targetPath));
+            let out = await less.render(content,options)
+            fs.writeFileSync(targetPath,out.css);
+        }catch(e){
+            return Promise.reject(e);
+        }
+        return next();
+    }
+}
